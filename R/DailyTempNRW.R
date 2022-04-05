92ff76015a96a5fd5da3226a6eb9a26d354d362b
Leandra Zanger <Leandra.Zanger@uni-duesseldorf.de>
2022-04-05 13:56:52 +0200

    Add Example datasets

---
+++ R/DailyTempNRW.R
@@ -0,0 +1,18 @@
+#' Daily Temperature Series at 9 stations in NRW.
+#'
+#'
+#' @format A list with components
+#' \describe{
+#' \item{TimeSeries}{ tibble (8,188 x 10): the first column `Datetime` contains
+#' the date of the observation, the columns 2 to 9 the observed daily maximum temperature
+#' at the respective station (see column names)
+#' }
+#' \item{SpatialCov}{Spatial covariates such as Station ID, Station name, elevation,
+#' latitude, longitude.}
+#' }
+#' @details
+#' The data was downloaded from the DWD.
+#' @source \url{https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/daily/kl/historical/}
+#'
+"DailyTempNRW"
+
